fd55f18c23257b0a316aeb4bdac084843c8b3d88
hernandezn <hernandezn@live.com>
2026-05-28 13:22:18 -0400

    Added package-info.java for feature-specific packages

---
+++ src/main/java/network/thenull/api/posts/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Handles operations regarding posts on the platform.
+ * 
+ * For example, this includes creating posts, updating posts, previewing posts 
+ * before submitting them to the platform, and retrieving posts for display.
+ */
+package network.thenull.api.posts;
---
+++ src/main/java/network/thenull/api/tags/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Handles operations regarding tags on the platform.
+ * 
+ * For example, this includes creating tags, updating tag definitions, 
+ * assigning tag aliases & implications, and categorizing tags.
+ */
+package network.thenull.api.tags;
